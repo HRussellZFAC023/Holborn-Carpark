8a36bd92cc2450e422c4b1b8746e5386ab71436d
alboiuvlad29 <alboiuvlad29@gmail.com>
2019-01-23 22:34:34 +0000

    First Client Prototype

---
+++ holborn.carparksystem.client/src/main/java/DataFetcher.java
@@ -0,0 +1,61 @@
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class DataFetcher<T> {
+    ObservableList<T> observableList;
+    ArrayList<DataFetcherListener<T>> listeners;
+    String url;
+    int delayMs;
+    boolean interruptFetching = false;
+    boolean forceFetched = false;
+    Runnable fetchingRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                if (interruptFetching) Thread.currentThread().interrupt();
+                try {
+                    if(!forceFetched)fetched(observableList);
+                    else{
+                        forceFetched = false;
+                    }
+                    Thread.sleep(delayMs);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    };
+    Thread fetchThread = new Thread(fetchingRunnable);
+
+    public DataFetcher(int delayMs){
+    this.delayMs = delayMs;
+    listeners = new ArrayList<>();
+    }
+
+    public void startFetching(){
+        interruptFetching = false;
+        fetchThread.setDaemon(true);
+        fetchThread.start();
+    }
+    public void interruptFetching(){
+    interruptFetching = true;
+    }
+    public void setDelay(int delayMs){
+         this.delayMs = delayMs;
+    }
+    public void forceFetch(){
+
+        fetched(observableList);
+    }
+    public void addListener(DataFetcherListener<T> listener){
+        listeners.add(listener);
+    }
+    public void fetched(ObservableList<T> list){
+        for (int i = 0; i < listeners.size() ; i++) {
+            listeners.get(i).onDataFetched(list);
+        }
+    }
+}
