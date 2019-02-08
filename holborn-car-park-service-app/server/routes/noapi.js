f90dc5eda536246088f99cee7c2dcee647f17e58
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2019-02-08 01:25:09 +0000

    fix - login page automatically redirects to Admin panel if session is active

--- holborn-car-park-service-app/server/routes/noapi.js
+++ holborn-car-park-service-app/server/routes/noapi.js
@@ -14,6 +14,7 @@ router.get('/', function (req, res) {
 });
 
 router.get('/login', function (req, res) {
+    if(req.session && req.session.user) res.redirect('/manager');
     res.sendFile('login.html', {root: path.join('public', 'protected', 'HTML')});
 });
 
