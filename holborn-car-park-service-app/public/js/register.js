97700100025b745687a11ae5b25cc404c28f58d9
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2019-02-10 14:40:20 +0000

    fix - stopped unnecessary function calls in register.js

--- holborn-car-park-service-app/public/js/register.js
+++ holborn-car-park-service-app/public/js/register.js
@@ -15,10 +15,10 @@ function validate() {
         url: '/register',
         type: 'POST',
         data: {
-            username:           $('#fld_username').val(),
-            email:              $('#fld_email').val(),
-            password:           $('#fld_password').val(),
-            confirm_password:   $('#fld_pwd_confirm').val()
+            username: $('#fld_username').val(),
+            email: $('#fld_email').val(),
+            password: $('#fld_password').val(),
+            confirm_password: $('#fld_pwd_confirm').val()
         },
         success: function (res, status, xhr) {
             if (res && res.redirect) {
@@ -26,57 +26,56 @@ function validate() {
             }
         },
         error: function (xhr, status, err) {
-            console.log(xhr.responseJSON)
-            internalError(xhr.responseJSON.type, xhr.responseJSON.message);
-            nameError(xhr.responseJSON.type, xhr.responseJSON.message);
-            emailError(xhr.responseJSON.type, xhr.responseJSON.message);
-            passwordError(xhr.responseJSON.type, xhr.responseJSON.message);
-            pwdConfirmError(xhr.responseJSON.type, xhr.responseJSON.message)
+            if (type === 'internal') {
+                internalError(xhr.responseJSON.type, xhr.responseJSON.message);
+            }
+            else if (type === 'invalid name' || type === 'space in name' || type === 'taken name' || type === 'long name') {
+                nameError(xhr.responseJSON.type, xhr.responseJSON.message);
+            }
+            else if (type === 'invalid email') {
+                emailError(xhr.responseJSON.type, xhr.responseJSON.message);
+            }
+            else if (type === 'invalid pwd' || type === 'short pwd' || type === 'disallowed pwd' || type === 'weak pwd') {
+                passwordError(xhr.responseJSON.type, xhr.responseJSON.message);
+            }
+            else if (type === 'match pwd') {
+                pwdConfirmError(xhr.responseJSON.type, xhr.responseJSON.message)
+            }
         }
     });
 }
 
 function internalError(type, message) {
-    if (type === 'internal') {
-        if (!$('#internal_error').length) {
-            $('#box').append("<p id='internal_error' class='help is-large is-danger'>" + message + "</p>");
-        }
+    if (!$('#internal_error').length) {
+        $('#box').append("<p id='internal_error' class='help is-large is-danger'>" + message + "</p>");
     }
 }
 
 function nameError(type, message) {
-    if (type === 'invalid name' || type === 'space in name' || type === 'taken name' || type === 'long name') {
-        if (!$('#name_error').length) {
-            $('#fld_username').addClass('is-danger');
-            $('#ctrl_username').append("<p id='name_error' class='help is-danger'>" + message + "</p>");
-        }
+    if (!$('#name_error').length) {
+        $('#fld_username').addClass('is-danger');
+        $('#ctrl_username').append("<p id='name_error' class='help is-danger'>" + message + "</p>");
     }
 }
 
 function emailError(type, message) {
-    if (type === 'invalid email') {
-        if (!$('#email_error').length) {
-            $('#fld_email').addClass('is-danger');
-            $('#ctrl_email').append("<p id='email_error' class='help is-danger'>" + message + "</p>");
-        }
+    if (!$('#email_error').length) {
+        $('#fld_email').addClass('is-danger');
+        $('#ctrl_email').append("<p id='email_error' class='help is-danger'>" + message + "</p>");
     }
 }
 
 function passwordError(type, message) {
-    if (type === 'invalid pwd' || type === 'short pwd' || type === 'disallowed pwd' || type === 'weak pwd') {
-        if (!$('#pwd_error').length) {
-            $('#fld_password').addClass('is-danger');
-            $('#ctrl_password').append("<p id='pwd_error' class='help is-danger'>" + message + "</p>");
-        }
+    if (!$('#pwd_error').length) {
+        $('#fld_password').addClass('is-danger');
+        $('#ctrl_password').append("<p id='pwd_error' class='help is-danger'>" + message + "</p>");
     }
 }
 
 function pwdConfirmError(type, message) {
-    if (type === 'match pwd') {
-        if (!$('#pwd_confirm_error').length) {
-            $('#fld_pwd_confirm').addClass('is-danger');
-            $('#ctrl_pwd_confirm').append("<p id='pwd_confirm_error' class='help is-danger'>" + message + "</p>");
-        }
+    if (!$('#pwd_confirm_error').length) {
+        $('#fld_pwd_confirm').addClass('is-danger');
+        $('#ctrl_pwd_confirm').append("<p id='pwd_confirm_error' class='help is-danger'>" + message + "</p>");
     }
 }
 
