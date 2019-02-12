a2fd17764bbc008d806bc50d6601af7236f07a82
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2019-02-12 15:36:49 +0000

    qoc - refactor socket.js to use async/await

--- holborn-car-park-service-app/server/sockets/socket.js
+++ holborn-car-park-service-app/server/sockets/socket.js
@@ -5,21 +5,21 @@ const socket_functions  = require('./socket_functions');
 
 module.exports = function (io) {
     io.on('connection', function (socket) {
-        var carpark_id = null;
+        let carpark_id = null;
         debug('Socket connected: ' + socket.handshake.address);
         socket_functions.authorise(socket, function (carparkid) {
             carpark_id = carparkid;
             socket.join(carpark_id);
             debug("joined in : " + carpark_id);
         });
-        socket.on('fetch-ticket', function (_id, callback) {
-            socket_functions.fetch_ticket_details(_id, carpark_id, callback);
+        socket.on('fetch-ticket', async function (_id, callback) {
+            await socket_functions.fetch_ticket_details(_id, carpark_id, callback);
         });
-        socket.on('fetch-carpark-details', function (callback) {
-            socket_functions.carpark_details_modified(carpark_id, callback)
+        socket.on('fetch-carpark-details', async function (callback) {
+            await socket_functions.carpark_details_modified(carpark_id, callback)
         });
-        socket.on('ticket-paid', function (paid, duration, date_out, _id) {
-            socket_functions.ticket_paid(paid, duration, date_out, _id, carpark_id);
+        socket.on('ticket-paid', async function (paid, duration, date_out, _id) {
+            await socket_functions.ticket_paid(paid, duration, date_out, _id, carpark_id);
         });
 
     });
