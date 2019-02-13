83848f7bfe055dfd360b37fcf84ef5d340add355
Henry <henry@therussellhouse.com>
2019-02-13 16:43:36 +0000

    fea - added onclcick to alll sidebar buttons

--- holborn-car-park-service-app/server/react/js/components/SideBar.js
+++ holborn-car-park-service-app/server/react/js/components/SideBar.js
@@ -6,26 +6,37 @@ class SideBar extends Component {
     return (
       <aside className="menu aside">
         <ul className="menu-list">
-          <li><a className="is-active"><i className="fas fa-home"/> Dashboard</a></li>
-          <li><a><i className="fas fa-file-contract"/> Report</a></li>
+          <li><a onClick={() => this.__setScene("Dash")}
+                 className="is-active"><i className="fas fa-home"/> Dashboard</a></li>
+          <li><a onClick={() => this.__setScene("Report")} >
+            <i className="fas fa-file-contract"/> Report</a></li>
         </ul>
         <p className="menu-label">
           Status update
         </p>
         <ul className="menu-list">
-          <li><a><i className="fas fa-car"/> Car Parks</a></li>
-          <li><a><i className="fas fa-ticket-alt"/>Car Parks</a></li>
+          <li><a onClick={() => this.__setScene("CarPark")} >
+            <i className="fas fa-car"/> Car Parks</a></li>
+          <li><a onClick={() => this.__setScene("Tickets")} >
+            <i className="fas fa-ticket-alt"/>Car Parks</a></li>
         </ul>
         <p className="menu-label">
           Admin Controls
         </p>
         <ul className="menu-list">
-          <li><a><i className="fas fa-smile-beam"/> Happy Hour</a></li>
-          <li><a><i className="fas fa-pound-sign"/> Pricing</a></li>
-          <li><a><i className="fas fa-business-time"/> Employees</a></li>
+          <li><a onClick={() => this.__setScene("Happy Hour")} >
+            <i className="fas fa-smile-beam"/> Happy Hour</a></li>
+          <li><a onClick={() => this.__setScene("Price")} >
+            <i className="fas fa-pound-sign"/> Pricing</a></li>
+          <li><a onClick={() => this.__setScene("Staff")} >
+            <i className="fas fa-business-time"/> Employees</a></li>
         </ul>
       </aside>
     )
+  }
+
+  __setScene(content) {
+    this.props.setScene(content);
   }
 }
 
