6d32e48e299f76e6e16ae3d0aa0b678dc1da95e3
alboiuvlad29 <alboiuvlad29@gmail.com>
2019-02-20 16:33:56 +0000

    feature - added support for themes (dark/light)

---
+++ holborn-car-park-service-app/public/js/preferredThemeScheme.js
@@ -0,0 +1,21 @@
+let storage_key = 'mode';
+let preferredThemeScheme = initPrefersColorScheme();
+if (localStorage.getItem(storage_key) === null) {
+    localStorage.setItem(storage_key, preferredThemeScheme.scheme);
+    console.log("init for " + localStorage.getItem(storage_key));
+} else {
+    console.log("else for " + localStorage.getItem(storage_key));
+    if (preferredThemeScheme.hasNativeSupport) localStorage.setItem(storage_key, preferredThemeScheme.scheme);
+    else {
+        preferredThemeScheme.scheme = localStorage.getItem(storage_key)
+    }
+}
+
+
+// this runs every time the preferred color scheme changes (by the OS or manually)
+preferredThemeScheme.onChange = function () {
+    // document.getElementById('preferred-color-scheme').lastChild.data = preferredThemeScheme.scheme;
+    localStorage.setItem('mode', preferredThemeScheme.scheme);
+};
+// let’s run it now
+preferredThemeScheme.onChange();
