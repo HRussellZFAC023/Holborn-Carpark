24543b6e80d28103d5ef8b3b156400c0bd4f1ee6
alboiuvlad29 <alboiuvlad29@gmail.com>
2019-02-21 00:38:56 +0000

    feature - fade when changing scenes

--- holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/ThemeProvider.java
+++ holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/ThemeProvider.java
@@ -1,6 +1,15 @@
 package uk.co.holborn.carparkclient;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class ThemeProvider {
     static Scene scene;
@@ -16,12 +25,42 @@ public class ThemeProvider {
     }
 
     public static void switchTheme(Themes theme) {
-        if (currentTheme == null) currentTheme = theme;
+        if (currentTheme == null) {
+            currentTheme = theme;
+        }
         if (currentTheme != theme) {
+            ImageView imgv = getImageFromNode(scene.getRoot(), 2);
+            AnchorPane root = (AnchorPane) scene.getRoot();
+            root.getChildren().add(imgv);
+            imgv.fitWidthProperty().bind(root.widthProperty());
+            imgv.fitHeightProperty().bind(root.heightProperty()
+            );
+            FadeTransition f = new FadeTransition();
+            f.setNode(root.getChildren().get(2));
+            f.setDuration(Duration.seconds(0.3));
+            f.setInterpolator(Interpolator.EASE_BOTH);
+            f.setFromValue(1.0);
+            f.setToValue(0.0);
+            f.setOnFinished(e->{
+                root.getChildren().remove(2);
+            });
+            f.play();
             scene.getStylesheets().remove(currentTheme.getStylesheetPath());
             currentTheme = theme;
         }
         scene.getStylesheets().add(theme.getStylesheetPath());
     }
+    public static ImageView getImageFromNode (Node node, double scale) {
+        final Bounds bounds = node.getLayoutBounds();
+        final WritableImage image = new WritableImage(
+                (int) Math.round(bounds.getWidth() * scale),
+                (int) Math.round(bounds.getHeight() * scale));
 
+        final SnapshotParameters spa = new SnapshotParameters();
+        spa.setTransform(javafx.scene.transform.Transform.scale(scale, scale));
+        final ImageView view = new ImageView(node.snapshot(spa, image));
+        view.setFitWidth(bounds.getWidth());
+        view.setFitHeight(bounds.getHeight());
+        return view;
+    }
 }
