bd4243937b8897bcbbd5de2914770be1c582df26
alboiuvlad29 <alboiuvlad29@gmail.com>
2019-02-23 18:09:29 +0000

    fix - setup method was called twice instead of once

--- holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/controllers/PaymentMethodsCashController.java
+++ holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/controllers/PaymentMethodsCashController.java
@@ -33,7 +33,6 @@ public class PaymentMethodsCashController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mc = MainViewController.getInstance();
-        setup();
     }
 
     @FXML
