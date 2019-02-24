db55a555e6d01e2a89f00f9dafe29c81f6b6063f
alboiuvlad29 <alboiuvlad29@gmail.com>
2019-02-24 16:14:54 +0000

    fix - updated PaymentMethodsContactlessController class to support the updated Sprite constructor

--- holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/controllers/PaymentMethodsContactlessController.java
+++ holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/controllers/PaymentMethodsContactlessController.java
@@ -2,9 +2,13 @@ package uk.co.holborn.carparkclient.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import uk.co.holborn.carparkclient.Animator;
 import uk.co.holborn.carparkclient.Sprite;
+import uk.co.holborn.carparkclient.Sprites;
 import uk.co.holborn.carparkclient.Ticket;
 
 import java.net.URL;
@@ -12,16 +16,12 @@ import java.util.ResourceBundle;
 
 public class PaymentMethodsContactlessController implements Initializable {
 
-    //    @FXML
-//    Button backButton;
-//    @FXML
-//    TextField inputAmount;
-//    @FXML
-//    Label price_due;
-//    @FXML
-//    Label infoText;
     @FXML
     ImageView imageView;
+    @FXML
+    VBox toggleVbox;
+    ToggleGroup toggleGroup;
+
     Ticket t;
     private MainViewController mc;
     private Sprite sprite;
@@ -30,9 +30,33 @@ public class PaymentMethodsContactlessController implements Initializable {
 
     public void initialize(URL location, ResourceBundle resources) {
         mc = MainViewController.getInstance();
-       //sprite = new Sprite(imageView, mc.getSpriteSheets().getImage(Sprites.PAYMENT_APPLE_PAY), 1000, 1000);
-        sprite.setSpritesCount(120);
-        sprite.setFPS(30);
+        toggleGroup = new ToggleGroup();
+        toggleGroup.getToggles().add((ToggleButton) toggleVbox.getChildren().get(0));
+        toggleGroup.getToggles().add((ToggleButton) toggleVbox.getChildren().get(1));
+        toggleGroup.getToggles().get(0).setUserData("Debit/Credit");
+        toggleGroup.getToggles().get(1).setUserData("Apple Pay");
+
+        toggleGroup.selectedToggleProperty().addListener((observableValue, oldToggle, newToggle) -> {
+                if (newToggle.getUserData().equals("Apple Pay")) {
+                    popInSprite(Sprites.PAYMENT_APPLE_PAY);
+                } else {
+                    popInSprite(Sprites.PAYMENT_CARD_CONTACTLESS);
+                }
+                ((ToggleButton) newToggle).setDisable(true);
+                if (oldToggle != null) ((ToggleButton) oldToggle).setDisable(false);
+        });
+    }
+
+    private void popInSprite(Sprites sp) {
+        if (sprite == null) sprite = new Sprite(imageView, mc.getSpriteSheets().getSpriteSettings(sp));
+        else {
+            sprite.pause();
+            sprite.setSpriteSettings(mc.getSpriteSheets().getSpriteSettings(sp));
+        }
+        Animator.nodePopIn(imageView, 0.4, e -> {
+            sprite.replay();
+        });
+
     }
 
     @FXML
@@ -41,63 +65,7 @@ public class PaymentMethodsContactlessController implements Initializable {
     }
 
     public void setup() {
-        sprite.resetView();
-        Animator.nodePopIn(imageView,0.6, e->{
-            sprite.replay();
-        });
+        toggleGroup.selectToggle(toggleGroup.getToggles().get(0));
     }
-
-//    public void pay() {
-//        double amount;
-//        try {
-//            amount = Double.parseDouble(inputAmount.getText());
-//        } catch (Exception e) {
-//            amount = 0;
-//        }
-//        due -= amount;
-//        paid += amount;
-//        if (due <= 0.0) {
-//            change = Math.abs(due);
-//            due = 0.0;
-//            inputAmount.setDisable(true);
-//            backButton.setVisible(false);
-//            if( change >0) setInfoText("Please take your change of £" + change);
-//            Thread t = new Thread(() -> {
-//                try {
-//                    mc.emitTicketPaid();
-//                    Thread.sleep(3000);
-//                    mc.sceneManager.changeTo(Scenes.LANDING);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            });
-//            t.setName("Thread-Sleep");
-//            t.setDaemon(true);
-//            t.start();
-//        }
-//        inputAmount.clear();
-//        updateUI();
-//    }
-//
-//    private void updateUI() {
-//        setAmoundDue("£" + due);
-//        setPaidAmount("£" + paid);
-//    }
-//
-//    private void setAmoundDue(String amount) {
-//        price_due.setText(amount);
-//        Animator.nodeFade(price_due, true);
-//    }
-//
-//    private void setPaidAmount(String amount) {
-//        price_paid.setText(amount);
-//        Animator.nodeFade(price_paid, true);
-//    }
-//
-//    private void setInfoText(String amount) {
-//        infoText.setText(amount);
-//        Animator.nodeFade(price_due, true);
-//    }
-
 
 }
