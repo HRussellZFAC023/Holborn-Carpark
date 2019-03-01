b01e13a3a3fb686f9914f8ab8d413e1ae1e4f4d5
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2019-03-01 01:20:42 +0000

    qol - add 0 in front of dates in graph

--- holborn-car-park-service-app/server/react/js/components/ReportSection.jsx
+++ holborn-car-park-service-app/server/react/js/components/ReportSection.jsx
@@ -21,14 +21,36 @@ class ReportSection extends Component {
             let current = start_d.getTime();
 
             while(current <= end_d.getTime()){
-                all.push(new Date(current).getDate() + "/" + (new Date(current).getMonth() + 1));
+                if(new Date(current).getDate() > 9 && (new Date(current).getMonth() + 1) > 9) {
+                    all.push(new Date(current).getDate() + "/" + (new Date(current).getMonth() + 1));
+                }
+                else if(new Date(current).getDate() <= 9 && (new Date(current).getMonth() + 1) > 9){
+                    all.push("0" + new Date(current).getDate() + "/" + (new Date(current).getMonth() + 1));
+                }
+                else if(new Date(current).getDate() > 9 && (new Date(current).getMonth() + 1) <= 9){
+                    all.push(new Date(current).getDate() + "/" + "0" + (new Date(current).getMonth() + 1));
+                }
+                else if(new Date(current).getDate() <= 9 && (new Date(current).getMonth() + 1) <= 9){
+                    all.push("0" + new Date(current).getDate() + "/" + "0" + (new Date(current).getMonth() + 1));
+                }
                 current += 7 * this.day;
                 this.graphSlots.push(current);
 
             }
 
-            if(end_d.getTime() > current){
-                all.push(end_d.getDate() + "/" + (end_d.getMonth() + 1));
+            if(end_d.getTime() > current) {
+                if (end_d.getDate() > 9 && end_d.getMonth() > 9) {
+                    all.push(end_d.getDate() + "/" + (end_d.getMonth() + 1));
+                }
+                else if (end_d.getDate() <= 9 && end_d.getMonth() > 9) {
+                    all.push("0" + end_d.getDate() + "/" + (end_d.getMonth() + 1));
+                }
+                else if (end_d.getDate() > 9 && end_d.getMonth() <= 9) {
+                    all.push(end_d.getDate() + "/" + "0" + (end_d.getMonth() + 1));
+                }
+                else if (end_d.getDate() <= 9 && end_d.getMonth() <= 9) {
+                    all.push("0" + end_d.getDate() + "/" + "0" + (end_d.getMonth() + 1));
+                }
                 this.graphSlots.push(end_d.getTime());
             }
 
@@ -47,7 +69,6 @@ class ReportSection extends Component {
                 }
             }
 
-            console.log(all)
             return all;
         };
     };
