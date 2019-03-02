8a8419fd9319ed809abf5f68d21505ce83ac10f6
alboiuvlad29 <alboiuvlad29@gmail.com>
2019-03-02 11:12:34 +0000

    qoc - commented finish controller

--- holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/controllers/FinishController.java
+++ holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/controllers/FinishController.java
@@ -14,6 +14,17 @@ import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * The finish controller handles the interactions of the finish UI.
+ * The controller will inform the user of any amount added by them without
+ * finishing the payment (and giving them back), inform the user of the change and if the ticket or
+ * smart card parking session has been paid.
+ * This controller has a countdown of a specified amount in {@link GlobalVariables}. After that countdown,
+ * the scene will automatically switch back to the landing page.
+ *
+ * @author Vlad Alboiu
+ * @version 1.0.1
+ */
 public class FinishController implements Initializable {
     @FXML
     Label infoText;
@@ -27,12 +38,18 @@ public class FinishController implements Initializable {
     Ticket ticket;
     Thread t;
 
+    /**
+     * Constructor
+     */
     public FinishController() {
         logger = LogManager.getLogger(getClass().getName());
         mc = MainViewController.getInstance();
     }
 
-
+    /**
+     * This method gets called after all the constructors have
+     * done their work to prepare the ui before displaying it.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         sprite = new Sprite(ticket_image, mc.getSpriteSheets().getSpriteSettings(Sprites.THANK_YOU));
@@ -41,16 +58,21 @@ public class FinishController implements Initializable {
 
     }
 
+    /**
+     * This method prepares the ui before showing, resenting the sprite,
+     * clearing the textfield, etc.
+     * This is called every time the scene manager switches to this scene.
+     */
     public void setup() {
         sprite.resetView();
         startFinishedChangeDelay();
         setMessage("Thank you for visiting us! Have a safe journey!");
         animateImageShow();
-         ticket= mc.ticket;
+        ticket = mc.ticket;
         setAditionalMessage("");
         if (ticket != null) {
             if (ticket.isPaidOnReceived()) {
-                setAditionalMessage("Your " + (ticket.isReceivedFromSmartcard() ? "smart card session" : "ticket") + " is already paid!");
+                setAditionalMessage("Your " + (ticket.isReceivedFromSmartcard() ? "smart card parking session" : "ticket") + " has been already paid!");
             } else if (ticket.isPaid()) {
                 if (ticket.getChange().compareTo(BigDecimal.ZERO) > 0) {
                     setAditionalMessage("Make sure to take your change of £" + ticket.getChange() + ".");
@@ -63,6 +85,9 @@ public class FinishController implements Initializable {
         }
     }
 
+    /**
+     * This method called by the next customer button will change to the landing page
+     */
     @FXML
     private void nextCustomer() {
         t.interrupt();
@@ -87,22 +112,37 @@ public class FinishController implements Initializable {
         t.start();
     }
 
+    /**
+     * Animate image showing in
+     */
     private void animateImageShow() {
         Animator.nodePopIn(ticket_image, 0.6, e -> {
             sprite.replay(1);
         });
     }
 
+    /**
+     * Inform the user with a message
+     * @param message the message to inform the user with
+     */
     private void setMessage(String message) {
         infoText.setText(message);
         Animator.nodeFade(infoText, true);
     }
 
+    /**
+     * Set the additional message to inform the user with
+     * @param message he message to inform the user with
+     */
     private void setAditionalMessage(String message) {
         aditionalInfo.setText(message);
         Animator.nodeFade(infoText, true);
     }
 
+    /**
+     * Emit a paid event to the socket that
+     * updates the values in the database  by the webservice
+     */
     void emitTicketPaid() {
         Object[] params = new Object[]{true, "" + ticket.getDuration(), "" + ticket.getDate_out(), "" + ticket.get_id(), "" + ticket.getPrice()};
         mc.getSocket().emit("ticket-paid", params);
