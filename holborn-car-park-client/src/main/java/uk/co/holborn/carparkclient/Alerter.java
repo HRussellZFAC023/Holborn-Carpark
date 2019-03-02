a52aacec5379c49ebb4acf0feb1e511eedf6d630
alboiuvlad29 <alboiuvlad29@gmail.com>
2019-03-02 01:55:24 +0000

    qoc - commented alert class

--- holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/Alerter.java
+++ holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/Alerter.java
@@ -8,9 +8,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
 
+/**
+ * Alerter class implements easy ways to alert user of different behaviour
+ * *
+ *
+ * @author Vlad Alboiu
+ * @version 1.0
+ */
 public class Alerter {
     private static String OS = System.getProperty("os.name").toLowerCase();
 
+    /**
+     * Show a customised alert
+     *
+     * @param title        the title of the alert
+     * @param header       the header of the alert
+     * @param content      the information displayed
+     * @param alertType    the type of the alert
+     * @param onCloseEvent event triggered once the alert gets closed
+     */
     public static void showAlert(String title, String header, String content, Alert.AlertType alertType, EventHandler<DialogEvent> onCloseEvent) {
         Alert a = getAlert(title, header, content, alertType);
         a.setOnCloseRequest(onCloseEvent);
@@ -18,6 +34,15 @@ public class Alerter {
 
     }
 
+    /**
+     * Get a new predefined alert model
+     *
+     * @param title     the title of the alert
+     * @param header    the header of the alert
+     * @param content   the information displayed
+     * @param alertType the type of the alert
+     * @return the alert
+     */
     public static Alert getAlert(String title, String header, String content, Alert.AlertType alertType) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
@@ -30,6 +55,14 @@ public class Alerter {
         dialogPane.setMinWidth(600);
         return alert;
     }
+
+    /**
+     * Display an unable to start alert that the app can't run because of the given reason
+     * and opens the running directory for configuration on a button prompt
+     *
+     * @param header  the header of the alert
+     * @param content the information displayed
+     */
     public static void showUnableToStartAlertAndOpenRunningDirectory(String header, String content) {
         ButtonType showLocation = new ButtonType("Show Location", ButtonBar.ButtonData.LEFT);
         ButtonType close = new ButtonType("Close", ButtonBar.ButtonData.CANCEL_CLOSE);
@@ -40,18 +73,30 @@ public class Alerter {
         if (result.isPresent() && result.get() == showLocation) {
             openRunningDirectory();
             System.exit(-1);
-        }else{
+        } else {
             System.exit(-1);
         }
     }
 
+    /**
+     * Show an unable to start alert
+     *
+     * @param header  the header of the alert
+     * @param content the information displayed
+     */
     public static void showUnableToStartAlert(String header, String content) {
-        showAlert("Unable to start", header, content, Alert.AlertType.ERROR, t->{
+        showAlert("Unable to start", header, content, Alert.AlertType.ERROR, t -> {
             System.exit(-1);
         });
 
     }
 
+    /**
+     * This method opens the the system's file explorer in the application's running directory
+     * <p>On mac, it opens Finder.</p>
+     * <p>On windows, it opens File Explorer</p>
+     * <p>On linux, it opens the default file explorer/p>
+     */
     public static void openRunningDirectory() {
         File currentJavaJarFile = new File(Alerter.class.getProtectionDomain().getCodeSource().getLocation().getPath());
         String currentJavaJarFilePath = currentJavaJarFile.getAbsolutePath();
@@ -65,18 +110,29 @@ public class Alerter {
         }
     }
 
+    /**
+     * Method that says whether or not the current platform is Windows
+     * @return true or false
+     */
     public static boolean isWindows() {
 
         return (OS.contains("win"));
 
     }
-
+    /**
+     * Method that says whether or not the current platform is MacOS
+     * @return true or false
+     */
     public static boolean isMac() {
 
         return (OS.contains("mac"));
 
     }
 
+    /**
+     * Method that says whether or not the current platform is Unix based system
+     * @return true or false
+     */
     public static boolean isUnix() {
 
         return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
