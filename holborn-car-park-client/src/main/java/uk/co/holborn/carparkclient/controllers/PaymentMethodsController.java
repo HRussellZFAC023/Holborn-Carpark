0b68f4852f1ea327405ae43ecb2b5a387020560b
alboiuvlad29 <alboiuvlad29@gmail.com>
2019-03-02 11:14:13 +0000

    qoc - commented payment methods controller

--- holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/controllers/PaymentMethodsController.java
+++ holborn-car-park-client/src/main/java/uk/co/holborn/carparkclient/controllers/PaymentMethodsController.java
@@ -9,7 +9,12 @@ import uk.co.holborn.carparkclient.Scenes;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
-
+/**
+ * The check controller handles the interactions of the payment methods UI.
+ *
+ * @author Vlad Alboiu
+ * @version 1.0.1
+ */
 public class PaymentMethodsController implements Initializable {
 
     @FXML
@@ -22,27 +27,50 @@ public class PaymentMethodsController implements Initializable {
     Label price;
     private MainViewController mc;
 
+    /**
+     * This method gets called after all the constructors have
+     * done their work to prepare the ui before displaying it.
+     *
+     * @param location
+     * @param resources
+     * @since 1.0.0
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mc = MainViewController.getInstance();
 
     }
 
+    /**
+     * Switch to cash payment screen
+     * @since 1.0.0
+     */
     @FXML
     private void cashPayment() {
         mc.sceneManager.changeTo(Scenes.PAYMENT_METHODS_CASH);
     }
 
+
     @FXML
     private void cardPayment() {
 
     }
-
+    /**
+     * Switch to compactness payment screen
+     * @since 1.0.1
+     */
     @FXML
     private void contactlessPayment() {
         mc.sceneManager.changeTo(Scenes.PAYMENT_METHODS_CONTACTLESS);
     }
 
+    /**
+     * This method prepares the ui before showing, resenting the sprite,
+     * clearing the textfield, etc.
+     * This is called every time the scene manager switches to this scene.
+     *
+     * @since 1.0.0
+     */
     public void setup() {
         if (mc.ticket.getAmountInTicketMachine().compareTo(BigDecimal.ZERO) > 0) {
             infoText.setText("Please pay the remaining amount");
@@ -54,6 +82,11 @@ public class PaymentMethodsController implements Initializable {
         price.setText("£" + (mc.ticket.getPrice().subtract(mc.ticket.getAmountInTicketMachine())));
     }
 
+    /**
+     * Go back to the previous scene
+     *
+     * @since 1.0.0
+     */
     @FXML
     private void back() {
         mc.sceneManager.changeTo(Scenes.FINISH);
