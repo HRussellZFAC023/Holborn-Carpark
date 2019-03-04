004a6bfa72c20ede200a3f0c219dd0122afc6869
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2019-03-04 04:50:57 +0000

    fix - old sweetalerts to sweetalerts2 migration

--- holborn-car-park-service-app/server/react/js/components/HappyHour.jsx
+++ holborn-car-park-service-app/server/react/js/components/HappyHour.jsx
@@ -1,84 +1,79 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
-import swal from 'sweetalert';
+import Swal from 'sweetalert2';
 
 class HappyHour extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-      selectedCarpark: { name: '' }
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            selectedCarpark: {name: ''}
+        };
+        this.dateChanged = this.dateChanged.bind(this);
+    }
+
+
+    dateChanged(d) {
+        this.setState({date: d});
+    }
+
+    confirm() {
+        Swal.fire({
+            title: 'Are you sure?',
+            html: 'You are about to create the following happy hour: <br />Date: <br />' + this.state.date + '<br />Carpark: <br />' + this.state.selectedCarpark.name + '<br />For one hour, the hourly rate will be reduced to zero.',
+            type: "warning",
+            showCancelButton: true
+        })
+            .then((yes) => {
+                if (yes.value) {
+                    Swal.fire({
+                        title: 'Happy Hour Activated!',
+                        type: 'success',
+                    }).then();
+                    this.send();
+                } else {
+                    Swal.fire({title: 'Succesfully Cancelled!', type: 'success'}).then();
+                }
+            });
     };
-    this.dateChanged = this.dateChanged.bind(this);
-  }
+
+    send() {
+        //send the data...
+    }
 
 
-  dateChanged(d) {
-    this.setState({ date: d });
-  }
-  
-  confirm() {
-    swal({
-      title: "Are you sure?",
-      text:
-        `You are about to create the following happy hour: \n
-       Date: \n ${this.state.date}\n 
-       Carpark: \n ${this.state.selectedCarpark.name}\n
-       For one hour, the hourly rate will be reduced to zero.
-      `,
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-      .then((yes) => {
-        if (yes) {
-          swal("Happy Hour Activated!", {
-            icon: "success",
-          });
-          this.send();
-        } else {
-          swal("Succesfully Cancelled!");
-        }
-      });
-  };
-
-  send() {
-    //send the data...
-  }
-  
-
-  render() {
-    return (
-      <main className="hero-body has-text-centered">
-        <h1 className="title is-1">Happy Hour!</h1>
-        <h2 className="subtitle is-4">Open the barriers and let the cars go free.</h2>
-        <div className="field">
-          <h1 className="content">Select time and date: </h1>
-          <DatePicker
-            selected={this.state.date}
-            onChange={this.dateChanged}
-            showTimeSelect
-            dateFormat="Pp"
-          />
-        </div>
-        <div className="field">
-          <h1 className="content">Select a car park:</h1>
-          <div class="select">
-            <select>
-              <option>Egham</option>
-              <option>Staines</option>
-            </select>
-          </div>
-        </div>
-        <br />
-        <div className="content field" >
-          <button onClick={() => this.confirm()} className="button is-large is-success">Start!</button>
-        </div>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main className="hero-body has-text-centered">
+                <h1 className="title is-1">Happy Hour!</h1>
+                <h2 className="subtitle is-4">Open the barriers and let the cars go free.</h2>
+                <div className="field">
+                    <h1 className="content">Select time and date: </h1>
+                    <DatePicker
+                        selected={this.state.date}
+                        onChange={this.dateChanged}
+                        showTimeSelect
+                        dateFormat="Pp"
+                    />
+                </div>
+                <div className="field">
+                    <h1 className="content">Select a car park:</h1>
+                    <div class="select">
+                        <select>
+                            <option>Egham</option>
+                            <option>Staines</option>
+                        </select>
+                    </div>
+                </div>
+                <br/>
+                <div className="content field">
+                    <button onClick={() => this.confirm()} className="button is-large is-success">Start!</button>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default HappyHour;
--- holborn-car-park-service-app/server/react/js/components/Pricing.jsx
+++ holborn-car-park-service-app/server/react/js/components/Pricing.jsx
@@ -1,81 +1,77 @@
-import React, { Component } from 'react';
-import { Slider } from 'material-ui-slider';
+import React, {Component} from 'react';
+import {Slider} from 'material-ui-slider';
 import "react-datepicker/dist/react-datepicker.css";
-import swal from 'sweetalert';
+import Swal from 'sweetalert2';
 
 class Pricing extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      price:0,
-      selectedCarpark: { name: '' }
+    constructor(props) {
+        super(props);
+        this.state = {
+            price: 0,
+            selectedCarpark: {name: ''}
+        };
+        this.priceChanged = this.dateChanged.bind(this);
+    }
+
+
+    dateChanged(p) {
+        this.setState({price: p});
+    }
+
+    confirm() {
+        Swal.fire({
+            title: 'Are you sure?',
+            html: 'You are about change the prices: <br />Price: <br />£' + this.state.price + '<br />Carpark: <br />' + this.state.selectedCarpark.name + '<br />The cost reflects how much is paid per hour.',
+            type: 'warning',
+            showCancelButton: true,
+        })
+            .then((yes) => {
+                if (yes) {
+                    Swal.fire({
+                        title: 'Price Changed!',
+                        type: 'success',
+                    }).then();
+                    this.send();
+                } else {
+                    Swal.fire({title: 'Succesfully Cancelled!', type: 'success'}).then();
+                }
+            });
     };
-    this.priceChanged = this.dateChanged.bind(this);
-  }
 
+    send() {
+        //send the data...
+    }
 
-  dateChanged(p) {
-    this.setState({ price: p });
-  }
-
-  confirm() {
-    swal({
-      title: "Are you sure?",
-      text:
-        `You are about change the prices: \n
-        Price: \n £${this.state.price}\n
-        Carpark: \n ${this.state.selectedCarpark.name}\n
-       The cost reflects how much is paid per hour.
-      `,
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-      .then((yes) => {
-        if (yes) {
-          swal("Happy Hour Activated!", {
-            icon: "success",
-          });
-          this.send();
-        } else {
-          swal("Succesfully Cancelled!");
-        }
-      });
-  };
-
-  send() {
-    //send the data...
-  }
-
-  render() {
-    return (
-      <main className="hero-body has-text-centered">
-        <h1 className="title is-1">Pricing</h1>
-        <h2 className="subtitle is-4">Here you can change the hourly fee.</h2>
-        <div className="field">
-          <h1  className="content">Input an hourly rate: £{this.state.price}</h1>
-          <Slider
-            value={this.state.value}
-            aria-labelledby="label"
-            onChange={this.priceChanged}
-          />
-        </div>
-        <div className="field">
-          <h1 className="content">Select a car park:</h1>
-          <div class="select">
-            <select>
-              <option>Egham</option>
-              <option>Staines</option>
-            </select>
-          </div>
-        </div><br />
-        <div className="content field" >
-          <button onClick={() => this.confirm()} className="button is-large is-success">Change!</button>
-        </div>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main className="hero-body has-text-centered">
+                <h1 className="title is-1">Pricing</h1>
+                <h2 className="subtitle is-4">Here you can change the hourly fee.</h2>
+                <div className="field">
+                    <h1 className="content">Input an hourly rate: £{this.state.price}</h1>
+                    <Slider
+                        value={this.state.value}
+                        aria-labelledby="label"
+                        onChange={this.priceChanged}
+                    />
+                </div>
+                <div className="field">
+                    <h1 className="content">Select a car park:</h1>
+                    <div class="select">
+                        <select>
+                            <option>Egham</option>
+                            <option>Staines</option>
+                        </select>
+                    </div>
+                </div>
+                <br/>
+                <div className="content field">
+                    <button onClick={() => this.confirm()} className="button is-large is-success">Change!</button>
+                </div>
+            </main>
+        );
+    }
 }
 
 export default Pricing;
